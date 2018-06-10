07d36579379d6d82e2c1f0da6f73f81ee67edd96
James Clancey <james.clancey@gmail.com>
2018-06-10 00:01:37 -0400

    fixing build script

--- simple_auth_flutter_example/ios/appcenter-post-clone.sh
+++ simple_auth_flutter_example/ios/appcenter-post-clone.sh
@@ -7,7 +7,6 @@ set -x
 cd ..
 git clone -b dev https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
-envman add --key PATH --value $PATH
 
 brew install --HEAD libimobiledevice
 brew install ideviceinstaller
