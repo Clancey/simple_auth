ad5aec4de2961bfcd4983b94f808b87c279460f3
James Clancey <james.clancey@gmail.com>
2018-06-11 11:05:41 -0400

    fixed build scripts

--- simple_auth_flutter_example/android/app/appcenter-post-clone.sh
+++ simple_auth_flutter_example/android/app/appcenter-post-clone.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 cd ..
 cd ..
-appcenter-post-clone.sh
+./appcenter-post-clone.sh
 flutter build android
--- simple_auth_flutter_example/ios/appcenter-post-clone.sh
+++ simple_auth_flutter_example/ios/appcenter-post-clone.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd ..
-appcenter-post-clone.sh
+./appcenter-post-clone.sh
 flutter build ios --release --no-codesign
