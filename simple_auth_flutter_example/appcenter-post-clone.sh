ef3cec0af86a587a69069fbe2400c95976453f87
James Clancey <james.clancey@gmail.com>
2018-06-11 11:01:00 -0400

    fixing android build

---
+++ simple_auth_flutter_example/appcenter-post-clone.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
+cd ..
+git clone -b dev https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
