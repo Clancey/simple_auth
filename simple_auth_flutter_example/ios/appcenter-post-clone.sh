0bf47dc5f3feddbe57376f825d9357dc23b6e0f1
James Clancey <james.clancey@gmail.com>
2018-06-11 11:09:41 -0400

    Hopefully fixed build scripts

--- simple_auth_flutter_example/ios/appcenter-post-clone.sh
+++ simple_auth_flutter_example/ios/appcenter-post-clone.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+# fail if any commands fails
+set -e
+# debug log
+set -x
+
 cd ..
-./appcenter-post-clone.sh
+git clone -b dev https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
+
 flutter build ios --release --no-codesign
