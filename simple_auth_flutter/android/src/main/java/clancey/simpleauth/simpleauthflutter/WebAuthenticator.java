dd83021b28b7f55f76b6db2fef06761aca96bba0
James Clancey <james.clancey@gmail.com>
2018-06-20 20:36:34 -0500

    worked on the web authenticator

--- simple_auth_flutter/android/src/main/java/clancey/simpleauth/simpleauthflutter/WebAuthenticator.java
+++ simple_auth_flutter/android/src/main/java/clancey/simpleauth/simpleauthflutter/WebAuthenticator.java
@@ -3,6 +3,7 @@ package clancey.simpleauth.simpleauthflutter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Dictionary;
+import java.util.HashMap;
 
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
@@ -30,22 +31,37 @@ public class WebAuthenticator {
     public boolean isCompleted;
     public boolean useEmbeddedBrowser;
     public EventChannel.EventSink eventSink;
-    public void checkUrl(URL url, boolean forceComplete)
+    public void checkUrl(URL url, final boolean forceComplete)
     {
-        //eventSink.success();
+        final String uri = url.toString();
+        eventSink.success( new HashMap<String, String>() {{
+            put("identifier",identifier);
+            put("url",uri);
+            put("forceComplete", forceComplete ? "true":"false");
+
+        }});
     }
     public void foundToken()
     {
-
+        isCompleted = true;
+        //TODO: proxy completed!
     }
 
     public void cancel()
     {
-
+        eventSink.success( new HashMap<String, String>() {{
+            put("identifier",identifier);
+            put("url","canceled");
+        }});
     }
 
-    public void failed(String error)
+    public void failed(final String error)
     {
-
+        eventSink.success( new HashMap<String, String>() {{
+            put("identifier",identifier);
+            put("url","error");
+            put("description", error);
+            put("forceComplete", "true");
+        }});
     }
 }
