6fa6d85efdeee91b771409ada8e007e8c1cd2243
James Clancey <james.clancey@gmail.com>
2018-10-10 11:46:49 -0400

    Fixed the tests

--- simple_auth_generator/test/simple_auth_generator_test.dart
+++ simple_auth_generator/test/simple_auth_generator_test.dart
@@ -1,28 +1,17 @@
 import 'package:simple_auth_generator/src/generator.dart';
 import 'package:test/test.dart';
 import 'dart:async';
-import 'package:analyzer/dart/ast/ast.dart';
 import 'package:dart_style/dart_style.dart' as dart_style;
 import 'package:path/path.dart' as p;
 import 'package:source_gen/source_gen.dart';
-import 'dart:io';
 
-import 'package:analyzer/file_system/physical_file_system.dart';
-import 'package:analyzer/src/dart/sdk/sdk.dart' show FolderBasedDartSdk;
-import 'package:analyzer/src/generated/engine.dart';
-import 'package:analyzer/src/generated/source.dart';
-import 'package:analyzer/src/source/package_map_resolver.dart';
-import 'package:analyzer/src/source/pub_package_map_provider.dart';
-import 'package:analyzer/src/generated/source_io.dart';
-import 'package:analyzer/file_system/file_system.dart' hide File;
+import 'analysis_utils.dart';
+import 'test_file_utils.dart';
 
 import 'dart:mirrors';
-
 import 'test_apis/services.dart';
 
 final _formatter = new dart_style.DartFormatter();
-
-CompilationUnit _compilationUnit;
 
 String _packagePathCache;
 String getPackagePath() {
@@ -39,44 +28,17 @@ String getPackagePath() {
   return _packagePathCache;
 }
 
+LibraryReader _library;
 void main() {
   setUpAll(() async {
-    var sdkPath = p.dirname(p.dirname(Platform.resolvedExecutable));
 
-    var resourceProvider = PhysicalResourceProvider.INSTANCE;
-    var sdk = new FolderBasedDartSdk(
-        resourceProvider, resourceProvider.getFolder(sdkPath));
-
-    var options = new AnalysisOptionsImpl()
-      ..analyzeFunctionBodies = false
-      ..previewDart2 = true;
-    var pubPackageMapProvider =
-        new PubPackageMapProvider(PhysicalResourceProvider.INSTANCE, sdk);
-    var packageMapInfo = pubPackageMapProvider.computePackageMap(
-        PhysicalResourceProvider.INSTANCE.getResource(getPackagePath())
-            as Folder);
-
-    AnalysisEngine.instance.processRequiredPlugins();
-    var context = AnalysisEngine.instance.createAnalysisContext()
-      ..analysisOptions = options
-      ..sourceFactory = new SourceFactory([
-        new DartUriResolver(sdk),
-        new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
-        new PackageMapUriResolver(
-            PhysicalResourceProvider.INSTANCE, packageMapInfo.packageMap)
-      ]);
-
-    var fileUri =
-        p.toUri(p.join(getPackagePath(), 'test', 'test_apis', 'services.dart'));
-    var source = context.sourceFactory.forUri2(fileUri);
-    var libElement = context.computeLibraryElement(source);
-    _compilationUnit = context.resolveCompilationUnit(source, libElement);
+  final path = testFilePath('test', 'test_apis');
+  _library = await resolveCompilationUnit(path);
   });
   var generator = new SimpleAuthGenerator();
 
   Future<String> runForElementNamed(String name) async {
-    var library = new LibraryReader(_compilationUnit.element.library);
-    var element = library.allElements.singleWhere((e) => e.name == name);
+    final element = _library.allElements.singleWhere((e) => e.name == name);
     var annotation = generator.typeChecker.firstAnnotationOf(element);
     var generated = await generator.generateForAnnotatedElement(
         element, new ConstantReader(annotation), null);
