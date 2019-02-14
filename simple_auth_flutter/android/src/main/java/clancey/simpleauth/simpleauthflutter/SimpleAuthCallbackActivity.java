018cdad18d9e32f16268b88c2ba846b93d6acd69
Riedl Kevin (WSDT) <kevin.riedl.privat@gmail.com>
2019-02-14 21:14:21 +0100

    Make compatible with Android X
    Annotations deprecated

--- simple_auth_flutter/android/src/main/java/clancey/simpleauth/simpleauthflutter/SimpleAuthCallbackActivity.java
+++ simple_auth_flutter/android/src/main/java/clancey/simpleauth/simpleauthflutter/SimpleAuthCallbackActivity.java
@@ -2,11 +2,10 @@ package clancey.simpleauth.simpleauthflutter;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 public class SimpleAuthCallbackActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         CustomTabsAuthenticator.onActivityResult(getIntent());
         finish();
