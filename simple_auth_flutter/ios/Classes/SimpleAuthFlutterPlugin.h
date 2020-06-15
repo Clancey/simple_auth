8233dd8879b8f7b2733ab372ce96665e16860a54
James Clancey <james.clancey@gmail.com>
2020-06-14 20:55:53 -0800

    Update SimpleAuthFlutterPlugin.h

--- simple_auth_flutter/ios/Classes/SimpleAuthFlutterPlugin.h
+++ simple_auth_flutter/ios/Classes/SimpleAuthFlutterPlugin.h
@@ -1,6 +1,5 @@
 #import <Flutter/Flutter.h>
 #import "WebAuthenticator.h"
-#import "WebAuthenticatorWindow.h"
 @interface SimpleAuthFlutterPlugin : NSObject<FlutterPlugin>
 + (BOOL)checkUrl:(NSURL *)url;
 @end
